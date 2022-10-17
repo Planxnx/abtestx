d23add331af34fe02a4f1d1f591eed98112d5e2c
Planxnx <planxthanee@gmail.com>
2022-10-17 07:14:14 +0700

    feat: create round-robin strategy

---
+++ roundrobin.go
@@ -0,0 +1,56 @@
+package abtestx
+
+import (
+	"sync/atomic"
+
+	"github.com/pkg/errors"
+	"github.com/samber/lo"
+)
+
+type RoundRobinTest struct {
+	ID       string       // Required
+	Callback func() error // Optional
+}
+
+type RoundRobin struct {
+	tests []*RoundRobinTest
+	next  uint64
+}
+
+// NewRoundRobin creates a new ab-test with round-robin strategy instance.
+func NewRoundRobin(tests []RoundRobinTest) Client {
+	if len(tests) == 0 {
+		panic(errors.New("tests is empty"))
+	}
+
+	t := lo.ToSlicePtr(tests)
+
+	for i, test := range t {
+		// validating
+		if test.ID == "" {
+			panic(errors.Errorf("test.ID[%v] is empty", i))
+		}
+	}
+
+	return &RoundRobin{
+		tests: t,
+		next:  0,
+	}
+}
+
+// Run using round-robin to choose test and execute the callback of the test.
+func (c *RoundRobin) Run() error {
+	_, callback := c.Pick()
+	if callback != nil {
+		return callback()
+	}
+	return nil
+}
+
+// Pick  using round-robin to choose test returns a test.
+func (c *RoundRobin) Pick() (id string, callback func() error) {
+	if test := c.tests[atomic.AddUint64(&c.next, 1)%uint64(len(c.tests))]; test != nil {
+		return test.ID, test.Callback
+	}
+	return "", nil
+}
